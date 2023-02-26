801d636db3ce4e43416ba1fb73d455b9b6b11e87
centinela1024tb <centinela1024tb@gmail.com>
2023-02-25 18:41:16 -0600

    [ADD] Se crean los controlers para login y registro, asi como sus routes y sus vistas

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,10 +1,17 @@
 
 const formularioLogin = (req, res) => {
   res.render('auth/login', {
-    autenticado: true
+
+  })
+}
+
+const formularioRegistro = (req, res) => {
+  res.render('auth/registro', {
+
   })
 }
 
 export {
-  formularioLogin
+  formularioLogin,
+  formularioRegistro
 }
