4cc30ead0a5181aa18faf8950850ebc143d6c657
rvaldez20 <rvaldez20@gmail.com>
2023-02-27 18:15:44 -0600

    [ADD] Se crea el modelo para usuarios
    => nombre, email, password, token y confirmado

---
+++ src/models/Usuario.js
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+
+import db from '../config/db.js';
+
+const Usuario = db.define('usuarios', {
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  token: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  confirmado: {
+    type: DataTypes.BOOLEAN
+  },
+})
+
+export default Usuario;
