ac93add322247bea64c9aa7738a60770ab89f65f
rvaldez20 <rvaldez20@gmail.com>
2023-03-05 00:09:33 -0600

    [ADD] Se empieza a implementar la validación del registro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,3 +1,5 @@
+import { check, validationResult } from 'express-validator';
+
 import Usuario from '../models/Usuario.js'
 
 
@@ -14,6 +16,28 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async(req, res) => {
+  // destructuraos la data del formulario
+  const { password, repetir_password } = req.body;
+
+  // Validacion
+  await check('nombre').notEmpty().withMessage('Nombre es obligatorio').run(req);
+  await check('email').isEmail().withMessage('Debe ser un email valido').run(req);
+  await check('password').isLength({ min: 6 }).withMessage('Password debe ser de al menos 6 caractere').run(req);
+  await check('repetir_password').equals(req.body.password).withMessage('Los password deben ser iguales').run(req);
+
+  let arrayErrores = validationResult(req);  // arrayErrores() retorna un array con los errors
+    
+  
+  // SI resultado esta no esta vacio mostramos los errores
+  if(!arrayErrores.isEmpty()) {
+    // hay errores de validacion
+    return res.render('auth/registro', {
+      page: "Crear Cuenta",
+      errores: arrayErrores.array(),
+    })
+  }
+
+  
   const usuario = await Usuario.create(req.body);
   res.json(usuario);
 }
