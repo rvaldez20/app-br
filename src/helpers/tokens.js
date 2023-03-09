00d215257406bdcc0be03334458ddb7bc46c9faa
rvaldez20 <rvaldez20@gmail.com>
2023-03-08 18:58:20 -0600

    [ADD] Se crea funcion para generar el JWT (json web token)

--- src/helpers/tokens.js
+++ src/helpers/tokens.js
@@ -1,6 +1,17 @@
+import jwt from 'jsonwebtoken'
 
+//! Funcion para generar un token aleatorio
 const generarId = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
 
+//! Funcion para generar un JWT
+const generarJWT = (id) => jwt.sign({ id }, process.env.JWT_SCERET, {
+    expiresIn: '1d'
+  })
+
+
+
+
 export {
-  generarId
+  generarId,
+  generarJWT,
 }
