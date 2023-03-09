5f0c2157ebd53cabf38cbd60c57f8ca6d327e30e
rvaldez20 <rvaldez20@gmail.com>
2023-03-09 15:50:25 -0600

    [ADD] Se agrega el css y js de leafleat, se configura en la vista para visualizar el mapa

--- src/assets/js/mapa.js
+++ src/assets/js/mapa.js
@@ -1,1 +1,13 @@
-alert('Soy un mapa.')
+(function() {
+    // const lat = 20.67444163271174;       guadalajara
+    // const lng = -103.38739216304566;
+    const lat = 24.0239822;
+    const lng = -104.6721137;
+    const mapa = L.map('mapa').setView([lat, lng ], 16);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+})()
