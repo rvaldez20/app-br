14b2f1d0d1681d5edbf03d3699f21f8476f91401
rvaldez20 <rvaldez20@gmail.com>
2023-03-10 17:05:54 -0600

    [ADD] Se implementa scope en modelo para filtrar campos en busquedas

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -30,6 +30,13 @@ const Usuario = db.define('usuarios', {
       const salt = await bcrypt.genSalt(10);
       usuario.password = await bcrypt.hash(usuario.password, salt);
     }
+  },
+  scopes: { // permite eliminar campos de una consulta: Modelo.scope('name_scope').find()
+    eliminarPassword: {
+      attributes: {
+        exclude: ['password', 'token', 'confirmado', 'createdAt', 'updatedAt']
+      }
+    }
   }
 })
 
