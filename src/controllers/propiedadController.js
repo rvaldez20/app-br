cd03571a38b5a2a7b093f86cece25ab9f4ac8d61
rvaldez20 <rvaldez20@gmail.com>
2023-03-10 12:31:00 -0600

    [ADD] Se crea router y controller para envio de la nueva propiedad

--- src/controllers/propiedadController.js
+++ src/controllers/propiedadController.js
@@ -3,7 +3,7 @@ import Categoria from '../models/Categoria.js';
 import Precio from '../models/Precio.js';
 
 
-//! Panel Admin
+//! Panel Admin (Home)
 const admin = (req, res) => {
   res.render('propiedades/admin', {
     page: 'Mis Propiedades',
@@ -11,9 +11,8 @@ const admin = (req, res) => {
   })
 }
 
-
+//! Formulario para crear una propiedad
 const crear = async (req, res) => {
-
   // Consultar Modelos de Precios y Categorias
   const [categorias, precios] = await Promise.all([
       Categoria.findAll(),
@@ -28,7 +27,14 @@ const crear = async (req, res) => {
   })
 }
 
+
+//! POST para envio y guarda la propiedad
+const guardarPropiedad = (req, res) => {
+
+}
+
 export {
   admin,
   crear,
+  guardarPropiedad,
 }
--- src/routes/propiedadesRoutes.js
+++ src/routes/propiedadesRoutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { 
   admin,
   crear,
+  guardarPropiedad
 } from '../controllers/propiedadController.js';
 
 const router = express.Router();
@@ -10,6 +11,7 @@ const router = express.Router();
 router.get('/mis-propiedades', admin);
 
 router.get('/propiedades/crear', crear);  // formulario para crear propiedad
+router.post('/propiedades/crear', guardarPropiedad);  // envia datos para crear propiedad
 
 
 
