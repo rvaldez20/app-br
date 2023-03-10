4553ae7220cf0fe90b17d0dacd53e910795d84bf
rvaldez20 <rvaldez20@gmail.com>
2023-03-09 21:17:11 -0600

    [ADD] Se agrega opcion por seeder de eliminar la data de Categorias y Precios

--- seed/seeder.js
+++ seed/seeder.js
@@ -12,18 +12,15 @@ const importarDatos = async () => {
   try {
     // !Autenticar
     await db.authenticate();
-    // const auth = db.authenticate();
 
     // !Generar las columnas
     await db.sync();
-    // const conexionDB = db.sync();
 
-    // !Insertar los datos
-    const importCategorias = Categoria.bulkCreate(categorias);
-    const importPrecios = Precio.bulkCreate(precios);
-
-    // ! Promise.all
-    await Promise.all([importCategorias, importPrecios]);
+    // ! Promise.all ejecuta al mismo tiempo las importacion de categorias y precios
+    await Promise.all([
+      Categoria.bulkCreate(categorias), 
+      Precio.bulkCreate(precios)
+    ]);
 
     console.log('Datos Importados Correctamente')
     process.exit();
@@ -34,7 +31,38 @@ const importarDatos = async () => {
   }
 }
 
+
+
+const eliminarDatos = async () => {
+  try {
+    // ! Promise.all ejecuta al mismo tiempo la eliminación de la data de categorias y precios
+    await Promise.all([
+      Categoria.destroy({ where: {}, truncate: true }), 
+      Precio.destroy({ where: {}, truncate: true })
+    ]);
+
+
+    // otra forma
+    // await db.sync({ force: true });
+
+    console.log('Datos Eliminados Correctamente')
+    process.exit();
+  } catch (error) {
+    console.log(error)
+    process.exit(1);  // finaliza el proceso
+  }
+}
+
+
+
+
 // si se le pasa el argumento -i en el script ejectua la fucncion para importar los datos
 if(process.argv[2] === '-i'){
   importarDatos();
+}
+
+// si se le pasa el argumento -e en el script ejectua la fucncion para eliminas data de 
+// categoria y precios
+if(process.argv[2] === '-e'){
+  eliminarDatos();
 }
