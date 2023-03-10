cb13f456b3441a04d8eaf5544d6d6e49d369938c
rvaldez20 <rvaldez20@gmail.com>
2023-03-10 16:36:43 -0600

    [FIX] se corrige un typeof

--- src/helpers/tokens.js
+++ src/helpers/tokens.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 const generarId = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
 
 //! Funcion para generar un JWT
-const generarJWT = (id) => jwt.sign({ id }, process.env.JWT_SCERET, {
+const generarJWT = (id) => jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '1d'
   })
 
