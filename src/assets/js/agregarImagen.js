762e96e81e0aac37ea301b99379a04189155ec69
rvaldez20 <rvaldez20@gmail.com>
2023-03-12 17:24:09 -0600

    [ADD] en la vista se pasa el token csrf y en el js se lee el token
    => se pasa via script: meta(name="csrf-token" content= csrfToken)
    => el token se pasa en los headers: {'CSRF-Token': token}

--- src/assets/js/agregarImagen.js
+++ src/assets/js/agregarImagen.js
@@ -1,14 +1,20 @@
 import {Dropzone } from 'dropzone';
 
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+// console.log(token)
+
 Dropzone.options.imagen = {
   dictDefaultMessage: 'Sube tus imágenes aquí',
   acceptedFiles: '.png,.jpg,.jpeg',
   maxFilesize: 5,
   maxFiles: 1,
   parallelUploads: 1,   // se recomienda que sea igual a maxFiles 
-  autoProcessQueue: false,
+  autoProcessQueue: true,
   addRemoveLinks: true,
   dictRemoveFile: 'Borrar Archivo',
-  dictMaxFilesExceeded: 'El limite es 1 archivo'
+  dictMaxFilesExceeded: 'El limite es 1 archivo',
+  headers: {
+    'CSRF-Token': token
+  }
 }
 
