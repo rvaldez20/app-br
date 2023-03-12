b3b92c9d99b4fc9ce229b33a88fc452bc280e6fd
rvaldez20 <rvaldez20@gmail.com>
2023-03-12 16:43:26 -0600

    [ADD] Se hace la configuración de dropzone leyendas, numero de archivos y forma de porcesar

--- src/assets/js/agregarImagen.js
+++ src/assets/js/agregarImagen.js
@@ -1,2 +1,14 @@
-import dropzone from 'dropzone';
+import {Dropzone } from 'dropzone';
+
+Dropzone.options.imagen = {
+  dictDefaultMessage: 'Sube tus imágenes aquí',
+  acceptedFiles: '.png,.jpg,.jpeg',
+  maxFilesize: 5,
+  maxFiles: 1,
+  parallelUploads: 1,   // se recomienda que sea igual a maxFiles 
+  autoProcessQueue: false,
+  addRemoveLinks: true,
+  dictRemoveFile: 'Borrar Archivo',
+  dictMaxFilesExceeded: 'El limite es 1 archivo'
+}
 
