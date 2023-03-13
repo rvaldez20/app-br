e4da1d00e7449e716f38d0bbd24b17081507bbfb
rvaldez20 <rvaldez20@gmail.com>
2023-03-12 20:06:40 -0600

    [ADD] ultimas config para subir imagenes, que no se autoprocesen y paramName: 'image

--- src/assets/js/agregarImagen.js
+++ src/assets/js/agregarImagen.js
@@ -9,12 +9,13 @@ Dropzone.options.imagen = {
   maxFilesize: 5,
   maxFiles: 1,
   parallelUploads: 1,   // se recomienda que sea igual a maxFiles 
-  autoProcessQueue: true,
+  autoProcessQueue: false,
   addRemoveLinks: true,
   dictRemoveFile: 'Borrar Archivo',
   dictMaxFilesExceeded: 'El limite es 1 archivo',
   headers: {
     'CSRF-Token': token
-  }
+  },
+  paramName: 'imagen',
 }
 
