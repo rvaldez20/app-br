bb846418de6fb771fbbca1755e19cda18787562a
rvaldez20 <rvaldez20@gmail.com>
2023-03-14 17:52:30 -0600

    [ADD] se agrega la route(POST) para guardar la actualización con los middlewares de validacion

--- src/routes/propiedadesRoutes.js
+++ src/routes/propiedadesRoutes.js
@@ -11,6 +11,7 @@ import {
   agregarImagen,
   almacenarImagen,
   editar,
+  guardarCambios,
 } from '../controllers/propiedadController.js';
 
 const router = express.Router();
@@ -18,7 +19,8 @@ const router = express.Router();
 router.get('/mis-propiedades', protegerRuta, admin);
 
 router.get('/propiedades/crear', protegerRuta, crear);              // formulario para crear propiedad
-router.post('/propiedades/crear', protegerRuta,
+router.post('/propiedades/crear', 
+  protegerRuta,
   body('titulo').notEmpty().withMessage('El titulo del anuncio es requerido'),
   body('descripcion')
     .notEmpty().withMessage('La descripción es requerida')
@@ -41,6 +43,19 @@ router.post('/propiedades/crear', protegerRuta,
     protegerRuta,
     editar
   );
+  router.post('/propiedades/editar/:id', 
+  protegerRuta,
+  body('titulo').notEmpty().withMessage('El titulo del anuncio es requerido'),
+  body('descripcion')
+    .notEmpty().withMessage('La descripción es requerida')
+    .isLength({ max: 200 }).withMessage('La descripción es muy extensa'),
+  body('categoria').isNumeric().withMessage('Es necesario seleccionar una categoria'),
+  body('precio').isNumeric().withMessage('Es necesario seleccionar un rango de precios'),
+  body('habitaciones').isNumeric().withMessage('Es necesario seleccionar la cantidad de habitaciones'),
+  body('estacionamiento').isNumeric().withMessage('Es necesario seleccionar la cantidad de estacionamientos'),
+  body('wc').isNumeric().withMessage('Es necesario seleccionar la cantidad de baños'),
+  body('lat').notEmpty().withMessage('Es necesario ubicar la propiedad en el mapa'),
+  guardarCambios);  
 
 
 
