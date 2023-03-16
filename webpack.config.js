9812e3f6ff7fb311391a5222db7c33d161ca12b3
rvaldez20 <rvaldez20@gmail.com>
2023-03-15 18:37:05 -0600

    [ADD] Se agrega a la configuración de webpack el archvio mostrarMapa.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ export default {
   mode: 'development',
   entry: {
     mapa: './src/assets/js/mapa.js',
-    agregarImagen: './src/assets/js/agregarImagen.js'
+    agregarImagen: './src/assets/js/agregarImagen.js',
+    mostrarMapa: './src/assets/js/mostrarMapa.js',
   },
   output: {
     filename: '[name].js',
